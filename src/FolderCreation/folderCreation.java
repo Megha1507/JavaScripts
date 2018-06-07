b862d659cda687b9cd9603799f55625217d0515d
Megha <meghs15sh@gmail.com>
2018-06-07 14:33:22 +0530

    DirectoryAndSubDirectory

--- src/FolderCreation/folderCreation.java
+++ src/FolderCreation/folderCreation.java
@@ -6,7 +6,35 @@ public class folderCreation {
 
 	public static void main(String[] args)
 	{
-		new File("D:\\JavaFilecreated").mkdir();
+// Create a Single Directory
+		File file = new File("D:\\JavaFilecreated");
+		
+		if(!file.exists())
+		{
+			if(file.mkdir())
+			{
+				System.out.println("Directory Created");
+				
+			}else
+			{
+				System.out.println("No Directory Created");
+			}
+		}
+		
+// Create Multiple Directory		
+		File MultiFile = new File("D:\\JavaFilecreated\\Android\\Cupcake");
+		
+		if(!MultiFile.exists())
+		{
+			if(MultiFile.mkdir())
+			{
+				System.out.println("Directory Created");
+				
+			}else
+			{
+				System.out.println("No Directory Created");
+			}
+		}
 	}
 
 }
